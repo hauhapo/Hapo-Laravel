e875557dc216b2d8563817bcb1c53c8615adb0d6
hapo-haunv <haunv@haposoft.com>
2020-02-10 15:10:54 +0700

    fix indent 3:10

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,20 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-	protected $fillable = ['name', 'description', 'start_time', 'end_time'];
+    protected $fillable = ['name', 'description', 'start_time', 'end_time'];
 
-		public function status()
-		{
-			return $this->hasOne(Status::class);
-		}
+    public function status()
+    {
+        return $this->hasOne(Status::class);
+    }
 
-		public function project()
-		{
-			return $this->belongsTo(Project::class);
-		}
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 
-		public function member()
-		{
-			return $this->belongsTo(Member::class);
-		}
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
 }
